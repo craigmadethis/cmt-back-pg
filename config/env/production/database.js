bbe7ba69905f8caabc61f7a0fec044b102a92973
Craig Shewry <craigshewry@gmail.com>
2022-04-03 12:50:38 +0100

    production db setup

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -13,16 +13,10 @@ module.exports = ({ env }) => ({
         database: config.database,
         username: config.user,
         password: config.password,
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        ssl: env.bool('DATABASE_SSL', false),
         },     
       },
-      // add this line
-      options: {
-        ssl: env.bool('DATABASE_SSL', false),
-      },
     },
-  },
 });
 
 
